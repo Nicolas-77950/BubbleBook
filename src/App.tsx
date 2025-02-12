cb2095f5f4ff51e0bff0062e96813ed7ea5ac0bb
Amandine GUISY <aguisy@yahoo.fr>
2025-02-12 15:50:56 +0100

    Dev/Resolution of a conflict with the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
 import React, { useState } from 'react';
 import Connexion from './pages/connexion';
->>>>>>> 2344717a79d46e95f5f7eaba43f1f8d36749f8ca
 import './App.css';
 import Register from './register'; 
 
 const App: React.FC = () => {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <Register /> 
-=======
     <div>
       <Connexion />
->>>>>>> 2344717a79d46e95f5f7eaba43f1f8d36749f8ca
     </div>
   )
 }
