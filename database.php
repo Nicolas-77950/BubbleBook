acafac24c9e85e45807ab0811c1f6b6f95bb3262
AmandineGUISY <aguisy@yahoo.fr>
2025-03-17 12:02:11 +0100

    Creation of the database class in order to be connected with the database

---
+++ database.php
@@ -0,0 +1,41 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'bubble';
+    private $username = 'root';
+    private $password = '1234';
+    private static $instance = null;
+    private $conn;
+
+    // Sécurisation with a private construct in order to stop directly the creation of instances
+    private function __construct() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Erreur de connexion : " . $exception->getMessage();
+        }
+    }
+
+    // Static methode to obtain the unique PDO connection
+    public static function getConnection() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance->conn;
+    }
+    public static function closeConnection() {
+        // Close the static instance connection if necessary
+        self::$instance = null;
+    
+        // If 'conn' is an instance property, use 'self' for a static method:
+        if (isset(self::$instance)) {
+            self::$instance->conn = null;
+        }
+    }
+    
+}
