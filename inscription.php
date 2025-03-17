08edde3c11ab69b905d2552d37c48552c0e43825
AmandineGUISY <aguisy@yahoo.fr>
2025-03-17 12:38:20 +0100

    Addition of the post method

--- inscription.php
+++ inscription.php
@@ -4,7 +4,7 @@
 <body class="min-h-screen flex flex-col">
   <!-- Section Principale -->
   <main class="bg-pink-100 flex-grow flex justify-center items-center p-16">
-    <form class="bg-white p-8 rounded-lg shadow-md w-96 space-y-4">
+    <form method="POST" class="bg-white p-8 rounded-lg shadow-md w-96 space-y-4">
       <h1 class="text-3xl font-bold text-center text-gray-900">Inscription</h1>
       <div>
         <label class="flex items-center">
