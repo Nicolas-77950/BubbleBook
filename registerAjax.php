8f2e9d44fa290127c995c4079c80296fcae7d8f1
AmandineGUISY <aguisy@yahoo.fr>
2025-03-17 14:50:13 +0100

    Inscription/ addition of verification for the input because required is not totally secured

--- registerAjax.php
+++ registerAjax.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once 'ClassUser.php';
 require_once 'database.php';
 
@@ -14,12 +13,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $errors = [];
 
+    // Verification of the input, if their are empty
     if (empty($email) || empty($name) || empty($first_name) || empty($password) || empty($confirm_password)) {
         $errors[] = "Tous les champs sont obligatoires.";
     }
 
+    // Verification that the password and confirm password are the same
     if ($password !== $confirm_password) {
         $errors[] = "Les mots de passe ne correspondent pas.";
     }
 
+    //if their is no errors, we can register
+    if (empty($errors)) {
+        try {
+
+            $pdo = Database::getConnection();
+            $user = new User($pdo);
+
+            // register of the user
+            [$statusCode, $message] = $user->registerUser($email, $password, $name, $first_name);
+
+            // Return the json
+            http_response_code($statusCode);
+            echo json_encode(['status' => $statusCode, 'message' => $message]);
+        } catch (PDOException $e) {
+
+            http_response_code(500);
+            echo json_encode(['status' => 500, 'message' => "Error with the databse : " . $e->getMessage()]);
+        }
+    } else {
+        // Return the valiators errors
+        http_response_code(400);
+        echo json_encode(['status' => 400, 'message' => $errors]);
+    }
+} else {
+    // if the request is not a post
+    http_response_code(405);
+    echo json_encode(['status' => 405, 'message' => 'Méthode non autorisée.']);
 }
