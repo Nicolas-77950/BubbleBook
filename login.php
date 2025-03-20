17ccfd0c7f79d2b2bfcd5211f0fa0fe0be8a37f8
AmandineGUISY <aguisy@yahoo.fr>
2025-03-20 10:54:06 +0100

    Login/ addition of the login.js script

--- login.php
+++ login.php
@@ -7,9 +7,21 @@
   <div class="flex-grow flex justify-center items-center p-36 bg-pink-100">
     <div class="bg-white rounded-lg shadow-md p-8 w-96">
       <h1 class="text-3xl font-bold text-center text-gray-900">Connexion</h1>
-      <form class="mt-4 space-y-4">
-        <input type="email" placeholder="Email" class="w-full bg-pink-50 border border-pink-300 rounded-lg px-4 py-2 focus:border-pink-500 focus:outline-none">
-        <input type="password" placeholder="Mot de passe" class="w-full bg-pink-50 border border-pink-300 rounded-lg px-4 py-2 focus:border-pink-500 focus:outline-none">
+      <form class="mt-4 space-y-4" method="POST">
+        <input
+          name="email" 
+          type="email" 
+          placeholder="Email" 
+          class="w-full bg-pink-50 border border-pink-300 rounded-lg px-4 py-2 focus:border-pink-500 focus:outline-none"
+          required
+        >
+        <input
+          name="password"
+          type="password" 
+          placeholder="Mot de passe"
+          class="w-full bg-pink-50 border border-pink-300 rounded-lg px-4 py-2 focus:border-pink-500 focus:outline-none"
+          required
+        >
         <button 
           type="submit" 
           class="w-full bg-pink-600 text-white rounded-full px-4 py-2 hover:bg-pink-600"
@@ -20,6 +32,7 @@
       <p class="mt-4 text-center text-gray-600">Aucun compte ? <a href="register.php" class="text-pink-500 hover:text-pink-600">Inscription</a></p>
     </div>
   </div>
+  <script src="Login/login.js"></script>
 </body>
 
 <?php 
