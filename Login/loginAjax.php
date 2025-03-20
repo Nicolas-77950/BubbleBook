686c6392090ce7251ae33383bcd1889056b9d478
AmandineGUISY <aguisy@yahoo.fr>
2025-03-20 10:14:48 +0100

    Login/ Recuperation of the post and creation of a session if the login is a sucess

---
+++ Login/loginAjax.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require_once '../database.php';
+require_once '../ClassUser.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = new User(Database::getConnection());
+    $res = $user -> login($email,$password);
+
+    if ($res[0] == 200) {
+        $user = $res[2];
+
+        // creation of the session
+        $_SESSION['email'] = $user ['email']?? '';
+        $_SESSION['groomer_id'] = $user ['groomer_id']?? '';
+        echo json_encode(['succes' => $res[0],'message'=> $res[1]]);
+    }else {
+        echo json_encode(['succes' => $res[0],'message'=> $res[1]]);
+    }
+    exit;
+}
+
